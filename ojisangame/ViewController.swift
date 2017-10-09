d438dbba5284d59295296ae8f48abe51c5defa3b
Tatsuya <t.mugen282@gmail.com>
2017-10-09 15:07:33 +0900

    Initial Commit

---
+++ ojisangame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ojisangame
+//
+//  Created by 福島達也 on 2017/10/09.
+//  Copyright © 2017年 TwitterAnimation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
