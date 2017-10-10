8ed98c3f215e73f7b0b65ee7b1c8aa37a7b7b659
Tatsuya <t.mugen282@gmail.com>
2017-10-11 01:33:19 +0900

    first commit

---
+++ ojisangame/photoCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  photoCollectionViewCell.swift
+//  ojisangame
+//
+//  Created by 福島達也 on 2017/10/09.
+//  Copyright © 2017年 TwitterAnimation. All rights reserved.
+//
+
+import UIKit
+
+class photoCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
